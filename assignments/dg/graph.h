cc483594d5fc3807541f959ae1cfce392d2f50ec
z5117173 <z5117173@ad.unsw.edu.au>
2019-07-27 12:31:50 +1000

    working on graph implementation

--- assignments/dg/graph.h
+++ assignments/dg/graph.h
@@ -1,17 +1,30 @@
 #ifndef ASSIGNMENTS_DG_GRAPH_H_
 #define ASSIGNMENTS_DG_GRAPH_H_
 
+#include <memory>
+
 namespace gdwg {
 
-template <typename N, typename E>
-class Graph {
- public:
-  class const_iterator {};
-
- private:
+template<typename T>
+// requires Relation<Less<T>, T>()
+struct CompareByValue {
+  bool operator()(const std::smart_ptr<T>& lhs, const std::smart_ptr<T>& rhs) const {
+    return *lhs < *rhs;
+  }
 };
 
-}  // namespace gdwg
+template<typename N, typename E>
+class Graph {
+ public:
+ private:
+  std::set<std::smart_ptr<Node>, CompareByValue<Node>> nodes_;
+  std::map<std::smart_ptr<Node>, ???, CompareByValue<Node>> nodes_;
+};
+
+// Define your graph_iterator here
+// Define your graph_const_iterator here
+
+}
 
 #include "assignments/dg/graph.tpp"
 
