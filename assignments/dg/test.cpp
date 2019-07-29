d212e123a5a108261e64049c436e82f8f007938e
z5117173 <z5117173@ad.unsw.edu.au>
2019-07-29 19:16:28 +1000

    figuring out how sets and ordering works for custom classes

--- assignments/dg/test.cpp
+++ assignments/dg/test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <set>
-
+#include <string>
 
 
 class Hello {
@@ -9,16 +9,30 @@ class Hello {
 	public:
 		int i;
 
-		bool operator* () {
-		  return this->i;
+		bool operator <(const Hello &hi) const {
+		    return this->i < hi.i;
 		}
+
+		friend std::ostream& operator<<(std::ostream& os, const Hello &hi);
+
+
 };
 
+std::ostream& operator<<(std::ostream& os, const Hello &hi)
+{
+    os << hi.i;
+    return os;
+}
+
+
+
+/*
 struct CompareByValue {
     bool operator()(const Hello& one, const Hello& two) const {
-        return *one < *two;
+        return one.i < two.i;
     }
 };
+ */
 
 int main() {
 	Hello a;
@@ -36,7 +50,7 @@ int main() {
     Hello g;
     g.i = 20;
 	//std::cout << hi.i << by.i << "\n";
-	std::set<<Hello>, CompareByValue> yo;
+	std::set<Hello> yo;
 	yo.insert(a);
 	yo.insert(b);
 	yo.insert(c);
@@ -44,6 +58,11 @@ int main() {
 	yo.insert(e);
 	yo.insert(f);
 	yo.insert(g);
+
+    for (const auto& hi : yo) {
+        std::cout << hi.i << "\n";
+    }
+
 
 
 
